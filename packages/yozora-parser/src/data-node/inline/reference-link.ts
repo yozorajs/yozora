c6bd32ffe1b9fb72c5a258aa95637084053e88b9
lemon-clown <me.clown.lemon@gmail.com>
2020-01-19 21:12:55 +0800

    :construction:  [core] feat(parser): add ReferenceLinkTokenizer

---
+++ packages/yozora-parser/src/data-node/inline/reference-link.ts
@@ -0,0 +1,197 @@
+import {
+  CharCode,
+  InlineDataNodeType,
+  DataNodeTokenPoint,
+  DataNodeTokenPosition,
+  DataNodeTokenFlankingGraph,
+  eatWhiteSpaces,
+  buildGraphFromThreeFlanking,
+} from '@yozora/core'
+import { InlineDataNodeTokenizer } from '../types'
+import { BaseInlineDataNodeTokenizer } from './_base'
+
+
+const T = InlineDataNodeType.REFERENCE_LINK
+type T = typeof T
+
+
+/**
+ * Lexical Analyzer for ReferenceLinkDataNode
+ *
+ * There are three kinds of reference links:
+ *  - full: A full reference link consists of a link text immediately followed by a link label
+ *    that matches a link reference definition elsewhere in the document.
+ *
+ *    A link label begins with a left bracket '[' and ends with the first right bracket ']' that
+ *    is not backslash-escaped. Between these brackets there must be at least one non-whitespace
+ *    character. Unescaped square bracket characters are not allowed inside the opening and
+ *    closing square brackets of link labels. A link label can have at most 999 characters
+ *    inside the square brackets.
+ *
+ *    One label matches another just in case their normalized forms are equal. To normalize
+ *    a label, strip off the opening and closing brackets, perform the Unicode case fold, strip
+ *    leading and trailing whitespace and collapse consecutive internal whitespace to a single
+ *    space. If there are multiple matching reference link definitions, the one that comes first
+ *    in the document is used. (It is desirable in such cases to emit a warning.)
+ *
+ *  - collapsed: A collapsed reference link consists of a link label that matches a link
+ *    reference definition elsewhere in the document, followed by the string []. The contents
+ *    of the first link label are parsed as inlines, which are used as the link’s text.
+ *    The link’s URI and title are provided by the matching reference link definition.
+ *    Thus, [foo][] is equivalent to [foo][foo].
+ *
+ *  - shortcut (not support): A shortcut reference link consists of a link label that matches
+ *    a link reference definition elsewhere in the document and is not followed by [] or a
+ *    link label. The contents of the first link label are parsed as inlines, which are used
+ *    as the link’s text. The link’s URI and title are provided by the matching link
+ *    reference definition. Thus, [foo] is equivalent to [foo][].
+ *
+ * @see https://github.github.com/gfm/#reference-link
+ */
+export class ReferenceLinkTokenizer
+  extends BaseInlineDataNodeTokenizer<T>
+  implements InlineDataNodeTokenizer<T> {
+  public readonly type = T
+
+  public match(content: string): DataNodeTokenFlankingGraph<T> {
+    const self = this
+    const leftFlanking = self.matchLeftFlanking(content)
+    const middleFlanking = self.matchMiddleFlanking(content)
+    const rightFlanking = self.matchRightFlanking(middleFlanking, content)
+    const isMatched = self.isMatched.bind(self, content)
+    const result = buildGraphFromThreeFlanking(
+      self.type, leftFlanking, middleFlanking, rightFlanking, isMatched)
+    return result
+  }
+
+  /**
+   * get all left borders (pattern: /\[/)
+   * @param content
+   */
+  protected matchLeftFlanking(content: string): DataNodeTokenPosition[] {
+    const results: DataNodeTokenPosition[] = []
+    const idx = (x: number) => content.charCodeAt(x)
+    for (let offset = 0, column = 1, line = 1; offset < content.length; ++offset, ++column) {
+      const c = idx(offset)
+      switch (c) {
+        case CharCode.BACK_SLASH:
+          ++offset
+          ++column
+          break
+        case CharCode.LINE_FEED:
+          column = 0
+          ++line
+          break
+        case CharCode.OPEN_BRACKET: {
+          const start: DataNodeTokenPoint = { offset, column, line }
+          const end: DataNodeTokenPoint = { offset: offset + 1, column: column + 1, line }
+          const result: DataNodeTokenPosition = { start, end }
+          results.push(result)
+          break
+        }
+      }
+    }
+    return results
+  }
+
+  /**
+   * get all middle borders (pattern: /\]\[/)
+   * @param content
+   */
+  protected matchMiddleFlanking(content: string): DataNodeTokenPosition[] {
+    const results: DataNodeTokenPosition[] = []
+    const idx = (x: number) => content.charCodeAt(x)
+    for (let offset = 0, column = 1, line = 1; offset < content.length; ++offset, ++column) {
+      const c = idx(offset)
+      switch (c) {
+        case CharCode.BACK_SLASH:
+          ++offset
+          ++column
+          break
+        case CharCode.LINE_FEED:
+          column = 0
+          ++line
+          break
+        case CharCode.CLOSE_BRACKET: {
+          if (idx(offset + 1) !== CharCode.OPEN_BRACKET) break
+          const start: DataNodeTokenPoint = { offset, column, line }
+          const end: DataNodeTokenPoint = { offset: offset + 2, column: column + 2, line }
+          const result: DataNodeTokenPosition = { start, end }
+          results.push(result)
+          ++column, ++offset
+          break
+        }
+      }
+    }
+    return results
+  }
+
+  /**
+   * get all middle borders (pattern: /\]/)
+   * @param content
+   */
+  protected matchRightFlanking(
+    middleFlanking: DataNodeTokenPosition[],
+    content: string,
+  ): DataNodeTokenPosition[] {
+    const results: DataNodeTokenPosition[] = []
+    const idx = (x: number) => content.charCodeAt(x)
+    for (const middle of middleFlanking) {
+      if (middle.end.offset >= content.length) break
+      const point = {
+        offset: middle.end.offset,
+        column: middle.end.column,
+        line: middle.end.line,
+      }
+
+      let flag = true
+      for (; flag && point.offset < content.length; ++point.offset, ++point.column) {
+        switch (idx(point.offset)) {
+          case CharCode.BACK_SLASH:
+            ++point.offset
+            ++point.column
+            break
+          case CharCode.LINE_FEED:
+            point.column = 0
+            ++point.line
+            break
+          case CharCode.OPEN_BRACKET:
+          case CharCode.CLOSE_BRACKET:
+            flag = false
+            --point.offset
+            --point.column
+            break
+        }
+      }
+
+      if (point.offset < content.length && idx(point.offset) === CharCode.CLOSE_BRACKET) {
+        const end = {
+          offset: point.offset + 1,
+          column: point.column + 1,
+          line: point.line,
+        }
+        const result: DataNodeTokenPosition = { start: point, end }
+        results.push(result)
+        continue
+      }
+    }
+    return results
+  }
+
+  /**
+   * A link label must contain at least one non-whitespace character
+   * @see https://github.github.com/gfm/#example-559
+   * @see https://github.github.com/gfm/#example-560
+   */
+  protected isMatched (
+    content: string,
+    left: DataNodeTokenPosition,
+    middle: DataNodeTokenPosition,
+  ): boolean {
+    if (left.end.offset >= middle.start.offset) return false
+    const start: DataNodeTokenPoint = { ...left.end }
+    eatWhiteSpaces(content, start)
+    if (start.offset >= middle.start.offset) return false
+    return true
+  }
+}
