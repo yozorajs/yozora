bbc077b192e13552c0f7c9dc93ddb00bceb35127
lemon-clown <me.clown.lemon@gmail.com>
2020-03-14 10:01:44 +0800

    :fire:  [parser] remove useless codes

--- packages/yozora-parser/script/test.ts
+++
@@ -1,6 +0,0 @@
-import { TextTokenizer } from '../src/data-node/inline/text'
-
-
-const tokenizer = new TextTokenizer({} as any, 1)
-const results = tokenizer.match('content')
-console.log('results:', results)
