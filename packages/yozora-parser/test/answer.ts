c2cea45d640f60440c646bbf3ec3530844ac6015
lemon-clown <me.clown.lemon@gmail.com>
2020-03-16 21:07:09 +0800

    :construction:  [parser] feat: update test utils

--- packages/yozora-parser/test/answer.ts
+++ packages/yozora-parser/test/answer.ts
@@ -1,5 +1,24 @@
 import path from 'path'
 import { TokenizerMatchTestCaseMaster } from './util/tokenizer-match-util'
+import { TokenizerParseTestCaseMaster } from './util/tokenizer-parse-util'
+
+
+async function answerMatchCases(caseRootDirectory: string, caseDirs: string[]) {
+  const caseMaster = new TokenizerMatchTestCaseMaster({ caseRootDirectory })
+  for (const caseDir of caseDirs) {
+    await caseMaster.scan(caseDir)
+  }
+  await caseMaster.answer()
+}
+
+
+async function answerParseCases(caseRootDirectory: string, caseDirs: string[]) {
+  const caseMaster = new TokenizerParseTestCaseMaster({ caseRootDirectory })
+  for (const caseDir of caseDirs) {
+    await caseMaster.scan(caseDir)
+  }
+  await caseMaster.answer()
+}
 
 
 /**
@@ -7,20 +26,27 @@ import { TokenizerMatchTestCaseMaster } from './util/tokenizer-match-util'
  */
 async function answer() {
   const caseRootDirectory = path.resolve(__dirname, 'cases/tokenizer')
-  const caseMaster = new TokenizerMatchTestCaseMaster({ caseRootDirectory })
-  await caseMaster.scan('inline/delete')
-  await caseMaster.scan('inline/emphasis')
-  await caseMaster.scan('inline/image')
-  await caseMaster.scan('inline/inline-code')
-  await caseMaster.scan('inline/inline-formula')
-  await caseMaster.scan('inline/inline-link')
-  await caseMaster.scan('inline/inline-html-comment')
-  await caseMaster.scan('inline/line-break')
-  await caseMaster.scan('inline/reference-image')
-  await caseMaster.scan('inline/reference-link')
-  await caseMaster.scan('inline/text')
-  await caseMaster.answer()
+  const caseDirs = [
+    'inline/delete',
+    'inline/emphasis',
+    'inline/image',
+    'inline/inline-code',
+    'inline/inline-formula',
+    'inline/inline-link',
+    'inline/inline-html-comment',
+    'inline/line-break',
+    'inline/reference-image',
+    'inline/reference-link',
+    'inline/text',
+  ]
+
+  await answerMatchCases(caseRootDirectory, caseDirs)
+  await answerParseCases(caseRootDirectory, caseDirs)
 }
 
 
 answer()
+
+
+import { dataNodeParser } from '@yozora/parser'
+// console.log(JSON.stringify(dataNodeParser.matchInlineData('~~alpha~~'), null , 2))
