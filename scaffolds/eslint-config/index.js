6393caa45101c5b455be13ce44b35e5f5bd4014f
lemon-clown <me.clown.lemon@gmail.com>
2020-03-29 22:47:02 +0800

    :art:  [eslint-config] refactor: rewrite with raw js

--- scaffolds/eslint-config/index.js
+++ scaffolds/eslint-config/index.js
@@ -1,4 +1,4 @@
-export = {
+module.exports = {
   'extends': [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint'
@@ -38,7 +38,7 @@ export = {
     'no-continue': 0,
     'no-cond-assign': 0,
     'no-inner-declarations': 0,
-    'no-param-reassign': ['error', { 'props': true  }],
+    'no-param-reassign': ['error', { 'props': true }],
     'no-plusplus': 0,
     'no-restricted-syntax': 0,
     'no-return-assign': 0,
