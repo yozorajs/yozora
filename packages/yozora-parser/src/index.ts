9533c8a421647982e97d03c96748c15f59c56e88
lemon-clown <me.clown.lemon@gmail.com>
2020-04-02 01:28:56 +0800

    :fire:  [yozora-parser] feat: remove inline tokenizers cause it has been migrate to ../../tokenizers/tokenizer-*

--- packages/yozora-parser/src/index.ts
+++ packages/yozora-parser/src/index.ts
@@ -1,16 +1,5 @@
 import { InlineDataNodeType, DataNode } from '@yozora/core'
 import { InlineDataNodeTokenizerContext } from './tokenizer/inline/_context'
-import { TextTokenizer } from './tokenizer/inline/text'
-import { DeleteTokenizer } from './tokenizer/inline/delete'
-import { EmphasisTokenizer } from './tokenizer/inline/emphasis'
-import { ImageTokenizer } from './tokenizer/inline/image'
-import { InlineCodeTokenizer } from './tokenizer/inline/inline-code'
-import { InlineFormulaTokenizer } from './tokenizer/inline/inline-formula'
-import { InlineHTMLCommentTokenizer } from './tokenizer/inline/inline-html-comment'
-import { InlineLinkTokenizer } from './tokenizer/inline/inline-link'
-import { LineBreakTokenizer } from './tokenizer/inline/line-break'
-import { ReferenceImageTokenizer } from './tokenizer/inline/reference-image'
-import { ReferenceLinkTokenizer } from './tokenizer/inline/reference-link'
 import { DataNodeTokenizerContext, DataNodeTokenizerConstructor } from './types/tokenizer'
 import { DataNodeTokenPosition, DataNodeTokenPointDetail } from './types/position'
 import { calcDataNodeTokenPointDetail } from './util/position'
@@ -18,17 +7,6 @@ import { calcDataNodeTokenPointDetail } from './util/position'
 
 export * from './tokenizer/inline/_base'
 export * from './tokenizer/inline/_context'
-export * from './tokenizer/inline/delete'
-export * from './tokenizer/inline/emphasis'
-export * from './tokenizer/inline/image'
-export * from './tokenizer/inline/inline-formula'
-export * from './tokenizer/inline/inline-code'
-export * from './tokenizer/inline/inline-link'
-export * from './tokenizer/inline/inline-html-comment'
-export * from './tokenizer/inline/line-break'
-export * from './tokenizer/inline/reference-image'
-export * from './tokenizer/inline/reference-link'
-export * from './tokenizer/inline/text'
 export * from './types/position'
 export * from './types/tokenizer'
 export * from './util/position'
@@ -102,18 +80,3 @@ export class DataNodeParser {
     return this.inlineContext.parse(content, codePoints, tokenPositions, s, t)
   }
 }
-
-
-export const dataNodeParser = new DataNodeParser(TextTokenizer)
-dataNodeParser
-  .useInlineDataTokenizer(1, DeleteTokenizer)
-  .useInlineDataTokenizer(1, EmphasisTokenizer)
-  .useInlineDataTokenizer(2, LineBreakTokenizer)
-  .useInlineDataTokenizer(3, ReferenceLinkTokenizer)
-  .useInlineDataTokenizer(3.1, ReferenceImageTokenizer)
-  .useInlineDataTokenizer(3, InlineLinkTokenizer)
-  .useInlineDataTokenizer(3.1, ImageTokenizer)
-  .useInlineDataTokenizer(4, InlineFormulaTokenizer)
-  .useInlineDataTokenizer(4, InlineCodeTokenizer)
-  .useInlineDataTokenizer(4, InlineHTMLCommentTokenizer)
-
