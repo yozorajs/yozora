9a1027afa6a5852ddbcf80fe05a7826b7e2aaf21
lemon-clown <me.clown.lemon@gmail.com>
2020-04-04 19:21:18 +0800

    :white_check_mark:  [parser-gfm] test: merge *test.ts

---
+++ packages/parser-gfm/test/suite.test.ts
@@ -0,0 +1,34 @@
+import path from 'path'
+import { it, describe, before } from 'mocha'
+import {
+  TokenizerMatchTestCaseMaster,
+  TokenizerParseTestCaseMaster,
+} from '@yozora/mocha-for-tokenizer'
+import { gfmDataNodeParser } from '../src'
+
+
+it('This is a required placeholder to allow before() to work', () => { })
+before(async function test() {
+  const match = gfmDataNodeParser.matchInlineData.bind(gfmDataNodeParser)
+  const parse = gfmDataNodeParser.parseInlineData.bind(gfmDataNodeParser)
+
+  const caseRootDirectory = path.resolve(__dirname, 'cases')
+  const matchTestCaseMaster = new TokenizerMatchTestCaseMaster(match, { caseRootDirectory })
+  const parseTestCaseMaster = new TokenizerParseTestCaseMaster(parse, { caseRootDirectory })
+
+  const caseDirs: string[] = ['inline/']
+  const tasks: Promise<any>[] = []
+  for (const caseDir of caseDirs) {
+    tasks.push(matchTestCaseMaster.scan(caseDir))
+    tasks.push(parseTestCaseMaster.scan(caseDir))
+  }
+  await Promise.all(tasks)
+
+  describe('match test cases', async function () {
+    matchTestCaseMaster.test()
+  })
+
+  describe('parse test cases', async function () {
+    parseTestCaseMaster.test()
+  })
+})
