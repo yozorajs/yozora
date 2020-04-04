20fb3fbc21975ebe821821a3b75dc2fb5353fcf5
lemon-clown <me.clown.lemon@gmail.com>
2020-04-04 20:55:58 +0800

    :white_check_mark:  [tokenizer-line-break] test: add test suites

---
+++ tokenizers/tokenizer-line-break/test/suite.test.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+import { it, describe, before } from 'mocha'
+import {
+  TokenizerMatchTestCaseMaster,
+  TokenizerParseTestCaseMaster,
+  mapInlineTokenizerToMatchFunc,
+  mapInlineTokenizerToParseFunc,
+} from '@yozora/mocha-for-tokenizer'
+import { LineBreakTokenizer } from '../src'
+
+
+it('This is a required placeholder to allow before() to work', () => { })
+before(async function test() {
+  const tokenizer = new LineBreakTokenizer({ priority: 1 })
+  const match = mapInlineTokenizerToMatchFunc(tokenizer)
+  const parse = mapInlineTokenizerToParseFunc(tokenizer)
+
+  const caseRootDirectory = path.resolve(__dirname)
+  const matchTestCaseMaster = new TokenizerMatchTestCaseMaster(match, { caseRootDirectory })
+  const parseTestCaseMaster = new TokenizerParseTestCaseMaster(parse, { caseRootDirectory })
+
+  const caseDirs: string[] = ['cases']
+  const tasks: Promise<any>[] = []
+  for (const caseDir of caseDirs) {
+    tasks.push(matchTestCaseMaster.scan(caseDir))
+    tasks.push(parseTestCaseMaster.scan(caseDir))
+  }
+  await Promise.all(tasks)
+
+  describe('match test cases', async function () {
+    matchTestCaseMaster.test()
+  })
+
+  describe('parse test cases', async function () {
+    parseTestCaseMaster.test()
+  })
+})
