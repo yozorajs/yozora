cec68cbb5d073d8f8191cf7e80f8ad9ddc105e9e
lemon-clown <me.clown.lemon@gmail.com>
2020-04-04 23:04:19 +0800

    :white_check_mark:  [tokenizer-emphasis] test: add test suites

---
+++ tokenizers/tokenizer-emphasis/test/answer.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+import {
+  TokenizerMatchTestCaseMaster,
+  TokenizerParseTestCaseMaster,
+  mapInlineTokenizerToMatchFunc,
+  mapInlineTokenizerToParseFunc,
+} from '@yozora/mocha-for-tokenizer'
+import { TextTokenizer } from '@yozora/tokenizer-text'
+import { EmphasisTokenizer } from '../src'
+
+
+/**
+ * create answer (to be checked)
+ */
+async function answer() {
+  const tokenizer = new EmphasisTokenizer({ priority: 1 })
+  const match = mapInlineTokenizerToMatchFunc(tokenizer, TextTokenizer)
+  const parse = mapInlineTokenizerToParseFunc(tokenizer, TextTokenizer)
+
+  const caseRootDirectory = path.resolve(__dirname)
+  const matchTestCaseMaster = new TokenizerMatchTestCaseMaster(match, { caseRootDirectory })
+  const parseTestCaseMaster = new TokenizerParseTestCaseMaster(parse, { caseRootDirectory })
+
+  const caseDirs: string[] = ['cases']
+  const tasks: Promise<any>[] = []
+  for (const caseDir of caseDirs) {
+    tasks.push(matchTestCaseMaster.scan(caseDir))
+    tasks.push(parseTestCaseMaster.scan(caseDir))
+  }
+  await Promise.all(tasks)
+
+  await matchTestCaseMaster.answer()
+  await parseTestCaseMaster.answer()
+}
+
+
+answer()
