bb7f54b77c20c59b0a602ee6ed96ab70ee7e28dd
lemon-clown <me.clown.lemon@gmail.com>
2020-04-04 22:48:16 +0800

    :white_check_mark:  [tokenizer-reference-link] test: add test suites

---
+++ tokenizers/tokenizer-reference-link/test/suite.test.ts
@@ -0,0 +1,38 @@
+import path from 'path'
+import { it, describe, before } from 'mocha'
+import {
+  TokenizerMatchTestCaseMaster,
+  TokenizerParseTestCaseMaster,
+  mapInlineTokenizerToMatchFunc,
+  mapInlineTokenizerToParseFunc,
+} from '@yozora/mocha-for-tokenizer'
+import { TextTokenizer } from '@yozora/tokenizer-text'
+import { ReferenceLinkTokenizer } from '../src'
+
+
+it('This is a required placeholder to allow before() to work', () => { })
+before(async function test() {
+  const tokenizer = new ReferenceLinkTokenizer({ priority: 1 })
+  const match = mapInlineTokenizerToMatchFunc(tokenizer, TextTokenizer)
+  const parse = mapInlineTokenizerToParseFunc(tokenizer, TextTokenizer)
+
+  const caseRootDirectory = path.resolve(__dirname)
+  const matchTestCaseMaster = new TokenizerMatchTestCaseMaster(match, { caseRootDirectory })
+  const parseTestCaseMaster = new TokenizerParseTestCaseMaster(parse, { caseRootDirectory })
+
+  const caseDirs: string[] = ['cases']
+  const tasks: Promise<any>[] = []
+  for (const caseDir of caseDirs) {
+    tasks.push(matchTestCaseMaster.scan(caseDir))
+    tasks.push(parseTestCaseMaster.scan(caseDir))
+  }
+  await Promise.all(tasks)
+
+  describe('match test cases', async function () {
+    matchTestCaseMaster.test()
+  })
+
+  describe('parse test cases', async function () {
+    parseTestCaseMaster.test()
+  })
+})
