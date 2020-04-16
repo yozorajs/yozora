92e9e68d867e6d9b259178065b1a47dd49cea15f
lemon-clown <me.clown.lemon@gmail.com>
2020-04-16 22:45:40 +0800

    :bulb:  [toeknizer-blockquote] style: update coments

--- tokenizers/tokenizer-blockquote/src/types.ts
+++ tokenizers/tokenizer-blockquote/src/types.ts
@@ -1,4 +1,4 @@
-import { BlockDataNode } from '@yozora/tokenizer-core'
+import { BlockDataNode, DataNodeParent } from '@yozora/tokenizer-core'
 
 
 /**
@@ -11,18 +11,29 @@ export type BlockquoteDataNodeType = typeof BlockquoteDataNodeType
 /**
  * data of BlockquoteDataNode
  */
-export interface BlockquoteDataNodeData {
+export interface BlockquoteDataNodeData extends DataNodeParent {
 
 }
 
 
 /**
+ * 引用块
+ * Blockquote (Parent) represents a section quoted from somewhere else.
  *
  * @example
  *    ````markdown
+ *    > Alpha bravo charlie.
  *    ````
  *    ===>
  *    ```js
+ *    {
+ *      type: 'BLOCKQUOTE',
+ *      children: [{
+ *        type: 'PARAGRAPH',
+ *        children: [{ type: 'TEXT', value: 'Alpha bravo charlie.' }]
+ *      }]
+ *    }
  *    ```
+ * @see https://github.com/syntax-tree/mdast#blockquote
  */
 export type BlockquoteDataNode = BlockDataNode<BlockquoteDataNodeType, BlockquoteDataNodeData>
