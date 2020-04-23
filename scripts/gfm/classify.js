8f5f87a305deee0320676dd7f4ea856715558ec3
lemon-clown <me.clown.lemon@gmail.com>
2020-04-23 21:24:28 +0800

    :construction:  chore: add scripts to fetch gfm examples

---
+++ scripts/gfm/classify.js
@@ -0,0 +1,87 @@
+const chalk = require('chalk')
+const fs = require('fs-extra')
+const path = require('path')
+
+
+class GFMExampleClassifier {
+  constructor (gfmExamples, baseDir, possibleMiddlePaths) {
+    this.gfmExamples = gfmExamples
+    this.baseDir = baseDir
+    this.numberLength = gfmExamples.length.toString().length
+    this.possibleMiddlePaths = possibleMiddlePaths
+  }
+
+  /**
+   *
+   * @param cases         {{ title: string, description: string, input: string, expectedHtml: string }[]}
+   * @param baseDir       {string}
+   * @param tokenizerName {string}
+   * @param groups        {{ name: string, start: number, end: number }[]}
+   */
+  classifyToTokenizer(tokenizerName, groups) {
+    const self = this
+    const getTokenizerPath = () => {
+      const resolvedTokenizerName = tokenizerName.replace(/^(tokenizer\-)?/, 'tokenizer-')
+      for (const mp of self.possibleMiddlePaths) {
+        const p = path.resolve(self.baseDir, mp, resolvedTokenizerName)
+        if (fs.existsSync(p)) return p
+      }
+      throw new Error(`cannot find source path for tokenizer(${ tokenizerName })`)
+    }
+
+    const tokenizerPath = getTokenizerPath()
+    for (const group of groups) {
+      const excluded = group.excluded || []
+      const groupDir = path.resolve(tokenizerPath, 'test/cases', group.name)
+      if (!fs.existsSync(groupDir)) fs.mkdirpSync(groupDir)
+      for (let i = group.start; i <= group.end; ++i) {
+        if (excluded.includes[i]) continue
+        const fileName = '#' + ('' + i).padStart(self.numberLength, '0') + '.json'
+        const caseFilePath = path.resolve(groupDir, fileName)
+        const gfmExample = self.gfmExamples[i]
+        const data = self.mapGFMExampleDataToCase(gfmExample)
+        const content = JSON.stringify(data, null, 2)
+        fs.writeFileSync(caseFilePath, content, 'utf-8')
+        console.log(chalk.green(`Add case ${ caseFilePath }`))
+      }
+    }
+
+    return self
+  }
+
+  /**
+   *
+   */
+  mapGFMExampleDataToCase(gfmExample) {
+    const result = {
+      title: gfmExample.title,
+      cases: [
+        {
+          description: gfmExample.description,
+          input: gfmExample.content,
+          htmlAnswer: gfmExample.expectedHtml,
+        }
+      ]
+    }
+    return result
+  }
+}
+
+
+const classifier = new GFMExampleClassifier(
+  require('./data.json'),
+  path.resolve(),
+  [
+    '', 'inline', 'block',
+    'tokenizers', 'tokenizers/inline', 'tokenizers/block'
+  ],
+)
+
+
+classifier
+  // .classifyToTokenizer('paragraph', [{
+  //   name: 'gfm',
+  //   start: 189,
+  //   end: 196,
+  //   excluded: [195],
+  // }])
---
+++ scripts/gfm/fetch.js
@@ -0,0 +1,67 @@
+/**
+ * 将 GFM 中的 example 元素解析成 JSON 数据
+ * @param exampleEl example 元素
+ */
+function parseGFMExample(exampleEl) {
+  if (exampleEl == null) return null
+
+  const exampleId = exampleEl.id || ''
+  const exampleNo = exampleId.replace(/^example\-(\d+)$/, '$1')
+  if (exampleNo.length < 0) {
+    console.error(`Bad exampleEl: id(${ exampleEl.id }). skipped`)
+    return null
+  }
+
+  const getCode = (column) => {
+    const codeEl = column.getElementsByTagName('code')[0]
+    const content = codeEl.innerText.replace(/\n$/, '')
+    return content
+  }
+
+  const getDescription = (el) => {
+    const prevEl = el.previousElementSibling
+    if (prevEl == null) return undefined
+    if (prevEl.tagName.toLowerCase() === 'p') {
+      return prevEl.innerText.replace(/:$/, '')
+    }
+    if (prevEl.classList.contains('example')) {
+      return getDescription(prevEl)
+    }
+  }
+
+  const { origin, pathname } = window.location
+  const columns = exampleEl.getElementsByClassName('column')
+  const result = {
+    title: `GFM#${ exampleNo } ${ origin }${ pathname }#${ exampleId }`,
+    description: getDescription(exampleEl),
+    content: getCode(columns[0]).replace(/→/g, '\t'),
+    expectedHtml: getCode(columns[1]).replace(/→/g, '\t'),
+  }
+  return result
+}
+
+
+function fetchGFMExamples(...exampleNos) {
+  const exampleEls = exampleNos.map(exampleNo => {
+    const id = 'example-' + exampleNo
+    const elementEl = document.getElementById(id)
+    return elementEl
+  })
+  let result = exampleEls.map(exampleEl => parseGFMExample(exampleEl))
+  if (result.length === 1) result = result[0]
+  return JSON.stringify(result, null, 2)
+}
+
+
+function fetchGFMExamplesInRange(left, right = left) {
+  const exampleNos = []
+  for (let i = left; i <= right; ++i) exampleNos.push(i)
+  return fetchGFMExamples(...exampleNos)
+}
+
+
+function fetchExamplesAll() {
+  const exampleEls = document.querySelectorAll('[id^=example]')
+  const result = Array.from(exampleEls).map(exampleEl => parseGFMExample(exampleEl))
+  return JSON.stringify(result, null, 2)
+}
