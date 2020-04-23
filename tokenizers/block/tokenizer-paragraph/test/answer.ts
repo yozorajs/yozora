151edc4df53efc00641315883a0238fca3137cb6
lemon-clown <me.clown.lemon@gmail.com>
2020-04-23 21:06:40 +0800

    :construction:  [tokenizer-paragraph] test: use mocha-for-tokenizer & update test cases

--- tokenizers/block/tokenizer-paragraph/test/answer.ts
+++ tokenizers/block/tokenizer-paragraph/test/answer.ts
@@ -4,7 +4,7 @@ import {
   TokenizerParseTestCaseMaster,
   mapBlockTokenizerToMatchFunc,
   mapBlockTokenizerToParseFunc,
-} from '@yozora/mocha-for-tokenizer'
+} from '@yozora/mocha-test-tokenizer'
 import { ParagraphTokenizer } from '../src'
 
 
@@ -12,23 +12,35 @@ import { ParagraphTokenizer } from '../src'
  * create answer (to be checked)
  */
 async function answer() {
-  const match = mapBlockTokenizerToMatchFunc(undefined, ParagraphTokenizer)
-  const parse = mapBlockTokenizerToParseFunc(undefined, ParagraphTokenizer)
-
   const caseRootDirectory = path.resolve(__dirname)
-  const matchTestCaseMaster = new TokenizerMatchTestCaseMaster(match, { caseRootDirectory })
-  const parseTestCaseMaster = new TokenizerParseTestCaseMaster(parse, { caseRootDirectory })
-
   const caseDirs: string[] = ['cases']
-  const tasks: Promise<any>[] = []
-  for (const caseDir of caseDirs) {
-    tasks.push(matchTestCaseMaster.scan(caseDir))
-    tasks.push(parseTestCaseMaster.scan(caseDir))
-  }
-  await Promise.all(tasks)
 
-  await matchTestCaseMaster.answer()
-  await parseTestCaseMaster.answer()
+  // answer match
+  const doMatchTask = async function () {
+    const match = mapBlockTokenizerToMatchFunc(undefined, ParagraphTokenizer)
+    const matchTestCaseMaster = new TokenizerMatchTestCaseMaster(match, { caseRootDirectory })
+    const tasks: Promise<any>[] = []
+    for (const caseDir of caseDirs) {
+      tasks.push(matchTestCaseMaster.scan(caseDir))
+    }
+    await Promise.all(tasks)
+    await matchTestCaseMaster.answer()
+  }
+
+  // answer parse
+  const doParseTask = async function () {
+    const parse = mapBlockTokenizerToParseFunc(undefined, ParagraphTokenizer)
+    const parseTestCaseMaster = new TokenizerParseTestCaseMaster(parse, { caseRootDirectory })
+    const tasks: Promise<any>[] = []
+    for (const caseDir of caseDirs) {
+      tasks.push(parseTestCaseMaster.scan(caseDir))
+    }
+    await Promise.all(tasks)
+    await parseTestCaseMaster.answer()
+  }
+
+  await doParseTask()
+  await doMatchTask()
 }
 
 
