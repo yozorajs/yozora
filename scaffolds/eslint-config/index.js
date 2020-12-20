f8923ca99d47b3db5c21a177289f981e273ae4ea
guanghechen <me.clown.lemon@gmail.com>
2020-12-20 12:27:57 +0800

    :construction:  improve(eslint-config): upgrade dependencies and rules

--- scaffolds/eslint-config/index.js
+++ scaffolds/eslint-config/index.js
@@ -1,58 +1,56 @@
+const customRules = require('./rule-custom')
+
+
 module.exports = {
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint'
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    'prettier'
-  ],
-  settings: {
-    'import/resolver': {
-      'node': {
-        'extensions': ['.js', '.ts']
+  root: true,
+  plugins: ['import'],
+  overrides: [
+    {
+      files: ['**/*{.ts,tsx}'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        ...customRules.tsRules,
       }
-    }
-  },
+    },
+  ],
   ignorePatterns: [
-    'lib/',
-    'dist/',
-    'build/',
-    'bundle/',
-    'target/',
-    'release/',
-    'node_modules/',
+    '**/test/cases/**',
+    '**/__test__/cases/**',
+    '**/node_modules/**',
+    '**/lib/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/target/**',
+    '**/vendor/**',
+    '**/release/**',
+    '**/example/**',
+    '**/demo/**',
+    '**/doc/**',
+    '**/tmp/**',
+    '**/__tmp__/**',
+    '**/script/**',
+    '**/coverage/**',
+    '**/*.styl.d.ts',
     '*.tsbuildinfo',
-    "rollup.config.js"
+    '.eslintrc.js',
+    'rollup.config.js',
   ],
   rules: {
-    'class-methods-use-this': 0,
-    'func-names': 0,
-    'import/prefer-default-export': 0,
-    'import/no-unresolved': 0,
-    'lines-between-class-members': 0,
-    'no-await-in-loop': 0,
-    'no-bitwise': 0,
-    'no-console': 0,
-    'no-continue': 0,
-    'no-cond-assign': 0,
-    'no-inner-declarations': 0,
-    'no-param-reassign': ['error', { 'props': true }],
-    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
-    'no-restricted-syntax': 0,
-    'no-return-assign': 0,
-    'no-throw-literal': 0,
-    'no-underscore-dangle': 0,
-    'prefer-destructuring': 0,
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'never'],
-    "@typescript-eslint/interface-name-prefix": 0,
-    '@typescript-eslint/no-empty-function': 0,
-    '@typescript-eslint/no-empty-interface': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/no-this-alias': 0,
-    '@typescript-eslint/no-use-before-define': 0
+    ...customRules.jsRules,
   }
 }
