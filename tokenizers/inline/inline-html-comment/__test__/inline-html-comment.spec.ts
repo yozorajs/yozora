f14f780ab6ca990c327f40c9d391fa4808ffc5c6
guanghechen <me.clown.lemon@gmail.com>
2020-12-28 21:33:50 +0800

    :white_check_mark:  test(inline-html-comment): use @yozora/jest-for-tokenzier instead of @yozora/mocha-test-tokenizer to make tests

---
+++ tokenizers/inline/inline-html-comment/__test__/inline-html-comment.spec.ts
@@ -0,0 +1,34 @@
+import path from 'path'
+import {
+  TokenizerMatchUseCaseMaster,
+  TokenizerParseUseCaseMaster,
+  mapInlineTokenizerToMatchFunc,
+  mapInlineTokenizerToParseFunc,
+} from '@yozora/jest-for-tokenizer'
+import { TextTokenizer } from '@yozora/tokenizer-text'
+import { InlineHtmlCommentTokenizer } from '../src'
+
+
+const tokenizer = new InlineHtmlCommentTokenizer({ priority: 1 })
+const fallbackTokenizer = new TextTokenizer({ priority: -1 })
+const { match } = mapInlineTokenizerToMatchFunc(fallbackTokenizer, tokenizer)
+const { parse } = mapInlineTokenizerToParseFunc(fallbackTokenizer, tokenizer)
+
+const caseRootDirectory = path.resolve(__dirname)
+const matchUseCaseMaster = new TokenizerMatchUseCaseMaster(match, caseRootDirectory)
+const parseUseCaseMaster = new TokenizerParseUseCaseMaster(parse, caseRootDirectory)
+
+const caseDirs: string[] = ['cases']
+for (const caseDir of caseDirs) {
+  matchUseCaseMaster.scan(caseDir)
+  parseUseCaseMaster.scan(caseDir)
+}
+
+
+describe('match test cases', function () {
+  matchUseCaseMaster.runCaseTree()
+})
+
+describe('parse test cases', function () {
+  parseUseCaseMaster.runCaseTree()
+})
