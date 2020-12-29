27426a6a51f7508e9aa9d794d319ff9aa1583cab
guanghechen <me.clown.lemon@gmail.com>
2020-12-29 19:48:34 +0800

    :white_check_mark:  test(setext-heading): use @yozora/jest-for-tokenzier instead of @yozora/mocha-test-tokenizer to make tests

---
+++ tokenizers/block/setext-heading/__test__/setext-heading.spec.ts
@@ -0,0 +1,39 @@
+import path from 'path'
+import {
+  TokenizerMatchUseCaseMaster,
+  TokenizerParseUseCaseMaster,
+  mapBlockTokenizerToMatchFunc,
+  mapBlockTokenizerToParseFunc,
+} from '@yozora/jest-for-tokenizer'
+import { ParagraphTokenizer } from '@yozora/tokenizer-paragraph'
+import { PhrasingContentDataNodeType } from '@yozora/tokenizercore-block'
+import { SetextHeadingTokenizer } from '../src'
+
+
+const tokenizer = new SetextHeadingTokenizer({ priority: 1 })
+const fallbackTokenizer = new ParagraphTokenizer({ priority: -1 })
+const { match } = mapBlockTokenizerToMatchFunc(fallbackTokenizer, tokenizer)
+const { parse } = mapBlockTokenizerToParseFunc(
+  fallbackTokenizer,
+  [PhrasingContentDataNodeType],
+  tokenizer)
+
+const caseRootDirectory = path.resolve(__dirname)
+const matchUseCaseMaster = new TokenizerMatchUseCaseMaster(match, caseRootDirectory)
+const parseUseCaseMaster = new TokenizerParseUseCaseMaster(parse, caseRootDirectory)
+
+const caseDirs: string[] = ['cases']
+for (const caseDir of caseDirs) {
+  matchUseCaseMaster.scan(caseDir)
+  parseUseCaseMaster.scan(caseDir)
+}
+
+
+describe('match test cases', function () {
+  matchUseCaseMaster.runCaseTree()
+})
+
+
+describe('parse test cases', function () {
+  parseUseCaseMaster.runCaseTree()
+})
