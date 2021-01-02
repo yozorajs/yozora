9c2ad4b15bae2b75aac68c3d14194bd487990bd3
guanghechen <me.clown.lemon@gmail.com>
2021-01-02 22:30:09 +0800

    :construction:  [template-tokenizer] improve: update boilerplates due to the change of @yozora/jest-for-tokenizer

--- jest.config.js
+++ jest.config.js
@@ -78,10 +78,10 @@ module.exports = {
   ],
   coverageThreshold: {
     'global': {
-      'branches': 60,
+      'branches': 40,
       'functions': 60,
       'lines': 60,
-      'statements': 60
+      'statements': 60,
     }
   },
   coverageReporters: [
