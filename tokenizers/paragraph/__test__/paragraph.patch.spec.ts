aa3a0f2ee55e1e756456b94c6bca85c63e23773a
guanghechen <me.clown.lemon@gmail.com>
2021-01-15 20:57:53 +0800

    :white_check_mark:  [tokenizer-paragraph] test: improve codes & udpate tests

---
+++ tokenizers/paragraph/__test__/paragraph.patch.spec.ts
@@ -0,0 +1,86 @@
+import type {
+  PhrasingContentLine,
+  PhrasingContentPostMatchPhaseState,
+} from '@yozora/tokenizercore-block'
+import type {
+  ParagraphMatchPhaseState,
+  ParagraphPostMatchPhaseState,
+} from '../src/types'
+import {
+  calcEndYastNodePoint,
+  calcEnhancedYastNodePoints,
+  calcStartYastNodePoint,
+} from '@yozora/tokenizercore'
+import {
+  PhrasingContentType,
+  mergeContentLines,
+} from '@yozora/tokenizercore-block'
+import { ParagraphTokenizer } from '../src/tokenizer'
+import { ParagraphType } from '../src/types'
+
+
+describe('paragraph patch test', function () {
+  const tokenizer = new ParagraphTokenizer({ priority: 0 })
+
+  const lines: ReadonlyArray<PhrasingContentLine> = [
+    {
+      firstNonWhiteSpaceIndex: 0,
+      nodePoints: calcEnhancedYastNodePoints('hello, world!'),
+    }
+  ]
+
+  const nextLines: ReadonlyArray<PhrasingContentLine> = [
+    {
+      firstNonWhiteSpaceIndex: 0,
+      nodePoints: calcEnhancedYastNodePoints('hello,'),
+    }
+  ]
+
+  const ms: ParagraphMatchPhaseState = {
+    type: ParagraphType,
+    lines: [...lines],
+  }
+
+  const pms: ParagraphPostMatchPhaseState = {
+    type: ParagraphType,
+    lines: [...lines],
+    position: {
+      start: calcStartYastNodePoint(lines[0].nodePoints, 0),
+      end: calcEndYastNodePoint(lines[0].nodePoints, lines[0].nodePoints.length - 1),
+    }
+  }
+
+  it('extractPhrasingContentLines', function () {
+    expect(tokenizer.extractPhrasingContentLines(ms))
+      .toEqual(lines)
+  })
+
+  it('buildMatchPhaseState', function () {
+    expect(tokenizer.buildMatchPhaseState(ms, nextLines))
+      .toEqual({ type: ParagraphType, lines: nextLines })
+  })
+
+  it('buildPostMatchPhaseState', function () {
+    expect(tokenizer.buildPostMatchPhaseState(pms, nextLines))
+      .toEqual({
+        type: ParagraphType,
+        lines: nextLines,
+        position: {
+          start: calcStartYastNodePoint(nextLines[0].nodePoints, 0),
+          end: calcEndYastNodePoint(nextLines[0].nodePoints, nextLines[0].nodePoints.length - 1),
+        }
+      })
+  })
+
+  it('buildPhrasingContent', function () {
+    const phrasingContentPS: PhrasingContentPostMatchPhaseState = {
+      ...pms,
+      type: PhrasingContentType,
+    }
+    expect(tokenizer.buildPhrasingContent(phrasingContentPS))
+      .toEqual({
+        type: PhrasingContentType,
+        contents: mergeContentLines(phrasingContentPS.lines)
+      })
+  })
+})
