6990c7a185ef6de7093c08598bc215cd24e00dcf
guanghechen <me.clown.lemon@gmail.com>
2021-01-16 11:17:28 +0800

    :art:  [parser-gfm] improve: export types from the dependent block tokenizers

---
+++ packages/parser-gfm/src/types.ts
@@ -0,0 +1,48 @@
+export { Blockquote, BlockquoteType } from '@yozora/tokenizer-blockquote'
+export { FencedCode, FencedCodeType } from '@yozora/tokenizer-fenced-code'
+export { Heading, HeadingType } from '@yozora/tokenizer-heading'
+export {
+  IndentedCode,
+  IndentedCodeType,
+} from '@yozora/tokenizer-indented-code'
+export {
+  LinkDefinition,
+  LinkDefinitionMetaData,
+  LinkDefinitionType,
+} from '@yozora/tokenizer-link-definition'
+export { List, ListType } from '@yozora/tokenizer-list'
+export {
+  ListBulletItem,
+  ListBulletItemType,
+} from '@yozora/tokenizer-list-bullet-item'
+export {
+  ListOrderedItem,
+  ListOrderedItemType,
+} from '@yozora/tokenizer-list-ordered-item'
+export {
+  ListTaskItem,
+  ListTaskItemType,
+} from '@yozora/tokenizer-list-task-item'
+export { Paragraph, ParagraphType } from '@yozora/tokenizer-paragraph'
+export {
+  SetextHeading,
+  SetextHeadingType,
+} from '@yozora/tokenizer-setext-heading'
+export {
+  Table,
+  TableAlignType,
+  TableCell,
+  TableCellType,
+  TableColumn,
+  TableRow,
+  TableRowType,
+  TableType,
+} from '@yozora/tokenizer-table'
+export {
+  ThematicBreak,
+  ThematicBreakType,
+} from '@yozora/tokenizer-thematic-break'
+export {
+  PhrasingContent,
+  PhrasingContentType,
+} from '@yozora/tokenizercore-block'
