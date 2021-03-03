b734b55fc0b8480a6b86499c8d94d1db36927561
guanghechen <me.guanghechen@gmail.com>
2021-03-03 21:19:25 +0800

    :wrench:  chore: use @guanghechen/eslint-config* & remove @barusu/tool-sort-imports

--- scaffolds/eslint-config/index.js
+++ scaffolds/eslint-config/index.js
@@ -1,39 +1,20 @@
-const customRules = require('./rule-custom')
-
 module.exports = {
   root: true,
-  env: { browser: true, commonjs: true, es6: true, jest: true, node: true },
-  globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
-  extends: ['eslint:recommended', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true },
+  extends: ['@guanghechen', 'plugin:jest/recommended', 'prettier'],
+  plugins: ['import'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
   },
-  plugins: ['import', 'prettier'],
-  rules: { ...customRules.jsRules },
+  rules: {},
   overrides: [
     {
-      files: ['**/*.ts', '**/*tsx'],
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'prettier',
-      ],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
-        project: './tsconfig.json',
-
-        // typescript-eslint specific options
-        warnOnUnsupportedTypeScriptVersion: true,
-        allowImportExportEverywhere: true,
-      },
-      plugins: ['import', 'prettier', '@typescript-eslint'],
-      rules: { ...customRules.jsRules, ...customRules.tsRules },
+      files: ['**/*.ts'],
+      extends: ['@guanghechen/ts', 'plugin:jest/recommended', 'prettier'],
+      rules: {},
     },
   ],
 }
