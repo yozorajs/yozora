6228c330ed6dc3113c671f56feaa91d47bff6d64
guanghechen <me.guanghechen@gmail.com>
2021-03-03 22:05:36 +0800

    :wrench:  chore: use @guanghechen/rollup-config instead of @barusu/rollup-config

--- tokenizers/text/rollup.config.js
+++ tokenizers/text/rollup.config.js
@@ -1,17 +1,11 @@
-import { createRollupConfig } from '@barusu/rollup-config'
-import path from 'path'
+import createRollupConfig from '@guanghechen/rollup-config'
 import manifest from './package.json'
-
-const resolvePath = p => path.resolve(__dirname, p)
-const paths = {
-  tsconfig: resolvePath('tsconfig.src.json'),
-}
 
 const config = createRollupConfig({
   manifest,
   pluginOptions: {
     typescriptOptions: {
-      tsconfig: paths.tsconfig,
+      tsconfig: 'tsconfig.src.json',
     },
   },
 })
