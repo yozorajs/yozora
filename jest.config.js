704881c323ab9fd060dd0427a71e54d0a83fe7ad
guanghechen <me.guanghechen@gmail.com>
2021-03-20 09:58:56 +0800

    :wrench:  chore: simplify jest config

--- jest.config.js
+++ jest.config.js
@@ -1,47 +1,7 @@
-const path = require('path')
-const { compilerOptions } = require('./tsconfig')
-
-const moduleNameMapper = {}
-for (const moduleName of Object.getOwnPropertyNames(compilerOptions.paths)) {
-  const paths = compilerOptions.paths[moduleName].map(p =>
-    path.resolve(__dirname, p),
-  )
-  let pattern = '^' + moduleName.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&') + '$'
-  moduleNameMapper[pattern] = paths.length === 1 ? paths[0] : paths
-}
+const { tsMonorepoConfig } = require('@guanghechen/jest-config')
 
 module.exports = {
-  bail: true,
-  verbose: true,
-  errorOnDeprecated: true,
-  roots: ['<rootDir>/src', '<rootDir>/__test__'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper,
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json',
-    },
-  },
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testURL: 'http://localhost/',
-  testEnvironment: 'node',
-  testRegex: '/(__test__)/[^/]+\\.spec\\.tsx?$',
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/coverage/',
-    '/(build|dist|lib|release|target|vendor)/',
-    '/(example|demo|doc)/',
-    '/(tmp|__tmp__)/',
-    '/script/',
-  ],
-  collectCoverage: false,
-  coverageDirectory: '<rootDir>/coverage/',
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/*.{js,jsx,ts,tsx}',
-  ],
+  ...tsMonorepoConfig(__dirname),
   coveragePathIgnorePatterns: [
     'packages/character/src/util/node-point.ts',
     'packages/parser-gfm/src/types.ts',
@@ -54,5 +14,4 @@ module.exports = {
       statements: 60,
     },
   },
-  coverageReporters: ['lcov', 'text', 'text-summary'],
 }
