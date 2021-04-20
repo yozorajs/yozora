3f518bc0cd07bb7107c2ced8f223b44120126e70
guanghechen <me.guanghechen@gmail.com>
2021-04-20 21:31:02 +0800

    :wrench:  chore: remove comments from bundled js

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,40 @@
 import createRollupConfig from '@guanghechen/rollup-config'
 import path from 'path'
 
-process.env.ROLLUP_SHOULD_SOURCEMAP = false
-
 async function rollupConfig() {
   const { default: manifest } = await import(path.resolve('package.json'))
-  const config = createRollupConfig({
-    manifest,
-    pluginOptions: {
-      typescriptOptions: { tsconfig: 'tsconfig.src.json' },
-      commonjsOptions: {
-        sourceMap: false,
+  const config = [
+    createRollupConfig({
+      manifest,
+      pluginOptions: {
+        typescriptOptions: {
+          tsconfig: 'tsconfig.src.json',
+          tsconfigOverride: {
+            compilerOptions: {
+              removeComments: false,
+              emitDeclarationOnly: true,
+            },
+          },
+        },
       },
-    },
-  })
+    }),
+    createRollupConfig({
+      manifest,
+      pluginOptions: {
+        typescriptOptions: {
+          tsconfig: 'tsconfig.src.json',
+          tsconfigOverride: {
+            compilerOptions: {
+              declaration: false,
+              declarationMap: false,
+              declarationDir: null,
+              removeComments: true,
+            },
+          },
+        },
+      },
+    }),
+  ]
   return config
 }
 
