a725d0f0736e6a915c98432bc11515d422d226e7
guanghechen <me.guanghechen@gmail.com>
2021-04-20 21:01:47 +0800

    :wrench:  chore: don't generate sourcemap while building packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
 import createRollupConfig from '@guanghechen/rollup-config'
 import path from 'path'
 
+process.env.ROLLUP_SHOULD_SOURCEMAP = false
+
 async function rollupConfig() {
   const { default: manifest } = await import(path.resolve('package.json'))
   const config = createRollupConfig({
     manifest,
     pluginOptions: {
       typescriptOptions: { tsconfig: 'tsconfig.src.json' },
+      commonjsOptions: {
+        sourceMap: false,
+      },
     },
   })
   return config
