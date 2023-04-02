6e7649056493038437e6b0f3f2d6150a573901ce
guanghechen <me.guanghechen@gmail.com>
2023-04-02 18:00:04 +0800

    :arrow_up:  chore: upgrade devDependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,22 @@
-import createRollupConfig from '@guanghechen/rollup-config'
-import path from 'path'
+import { createRollupConfig, tsPresetConfigBuilder } from '@guanghechen/rollup-config'
+import path from 'node:path'
 
 export default async function rollupConfig() {
-  const { default: manifest } = await import(
-    path.resolve('package.json'),
-    { assert: { type: 'json' } },
-  )
+  const { default: manifest } = await import(path.resolve('package.json'), {
+    assert: { type: 'json' },
+  })
   const config = await createRollupConfig({
     manifest,
-    pluginOptions: {
-      typescriptOptions: { tsconfig: 'tsconfig.src.json' },
+    env: {
+      sourcemap: false,
     },
+    presetConfigBuilders: [
+      tsPresetConfigBuilder({
+        typescriptOptions: {
+          tsconfig: 'tsconfig.src.json',
+        },
+      }),
+    ],
   })
   return config
 }
