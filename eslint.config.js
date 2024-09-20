e509173eec7d08dfe3e89cabe19359d3927243d6
guanghechen <me.guanghechen@gmail.com>
2024-09-20 15:55:33 +0800

    :wrench:  chore: upgrade devDependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,24 +3,12 @@ import ghcConfigs from '@guanghechen/eslint-config'
 export default [
   {
     ignores: [
-      '.DS_Store',
-      '**/*.hbs',
       '.vscode/',
-      '**/.husky/',
-      '**/.nx/',
-      '**/.git/',
-      '**/.yarn/',
       '**/__tmp__/',
       '**/__test__/cases/',
-      '**/__test__/fixtures/',
-      '**/coverage/',
-      '**/dist/',
       '**/doc/',
       '**/example/',
-      '**/lib/',
-      '**/node_modules/',
       '**/resources/',
-      '**/test/',
       'packages/character/src/constant/entity.ts',
     ],
   },
@@ -28,23 +16,14 @@ export default [
   {
     files: ['packages/ast/src/nodes/**/*.ts'],
     rules: {
-      '@typescript-eslint/no-redeclare': 0,
+      '@typescript-eslint/no-redeclare': 'off',
     },
   },
   {
     files: ['**/src/**/*.ts'],
     rules: {
-      '@typescript-eslint/no-unnecessary-type-arguments': 0,
-      'import/no-named-as-default': 0,
-    },
-  },
-  {
-    files: ['**/__test__/**/*.spec.{js,cjs,mjs,ts,cts,mts}'],
-    rules: {
-      '@typescript-eslint/ban-ts-comment': 0,
-      'import/no-extraneous-dependencies': 0,
-      'import/no-named-as-default': 0,
-      'jest/expect-expect': 0,
+      '@typescript-eslint/no-unnecessary-type-arguments': 'off',
+      'import/no-named-as-default': 'off',
     },
   },
 ]
